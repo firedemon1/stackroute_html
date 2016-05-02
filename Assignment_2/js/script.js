7e67d9f64bd3fff19802aa990eb2f5b87b743eb4
firedemon1 <codingdeepak@gmail.com>
2016-05-02 14:22:51 +0530

    Bootstrap extra features implementation

--- Assignment_2/js/script.js
+++ Assignment_2/js/script.js
@@ -1,119 +1,19 @@
-function myFunction1() {
-  var innerHTML = document.getElementById("button1").innerHTML;
+function myFunction(button) {
+  var innerHTML = document.getElementById(button).innerHTML;
+  console.log(button+"acac");
   if(innerHTML == "More") {
-    document.getElementById("button1").innerHTML = "Less";
+    document.getElementById(button).innerHTML = "Less";
   } else {
-    document.getElementById("button1").innerHTML = "More";
+    document.getElementById(button).innerHTML = "More";
   }
+  var num = button.substring(6, 7);
+  console.log(num);
+  $("p#less"+num).toggle();
+  $("p.more"+num).toggle();
 }
-function myFunction2() {
-  var innerHTML = document.getElementById("button2").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button2").innerHTML = "Less";
-  } else {
-    document.getElementById("button2").innerHTML = "More";
-  }
-}
-function myFunction3() {
-  var innerHTML = document.getElementById("button3").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button3").innerHTML = "Less";
-  } else {
-    document.getElementById("button3").innerHTML = "More";
-  }
-}
-function myFunction4() {
-  var innerHTML = document.getElementById("button4").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button4").innerHTML = "Less";
-  } else {
-    document.getElementById("button4").innerHTML = "More";
-  }
-}
-function myFunction5() {
-  var innerHTML = document.getElementById("button5").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button5").innerHTML = "Less";
-  } else {
-    document.getElementById("button5").innerHTML = "More";
-  }
-}
-function myFunction6() {
-  var innerHTML = document.getElementById("button6").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button6").innerHTML = "Less";
-  } else {
-    document.getElementById("button6").innerHTML = "More";
-  }
-}
-function myFunction7() {
-  var innerHTML = document.getElementById("button7").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button7").innerHTML = "Less";
-  } else {
-    document.getElementById("button7").innerHTML = "More";
-  }
-}
-function myFunction8() {
-  var innerHTML = document.getElementById("button8").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button8").innerHTML = "Less";
-  } else {
-    document.getElementById("button8").innerHTML = "More";
-  }
-}
-function myFunction9() {
-  var innerHTML = document.getElementById("button9").innerHTML;
-  if(innerHTML == "More") {
-    document.getElementById("button9").innerHTML = "Less";
-  } else {
-    document.getElementById("button9").innerHTML = "More";
-  }
-}
+
 $(document).ready(function(){
-  $("p#more1").hide();
-  $("p.more2").hide();
-  $("p#more3").hide();
-  $("p.more4").hide();
-  $("p#more5").hide();
-  $("p#more6").hide();
-  $("p#more7").hide();
-  $("p.more8").hide();
-  $("p.more9").hide();
-  $("button#button1").click(function(){
-    $("p#less1").toggle();
-    $("p#more1").toggle();
-  });
-  $("button#button2").click(function(){
-    $("p#less2").toggle();
-    $("p.more2").toggle();
-  });
-  $("button#button3").click(function(){
-    $("p#less3").toggle();
-    $("p#more3").toggle();
-  });
-  $("button#button4").click(function(){
-    $("p#less4").toggle();
-    $("p.more4").toggle();
-  });
-  $("button#button5").click(function(){
-    $("p#less5").toggle();
-    $("p#more5").toggle();
-  });
-  $("button#button6").click(function(){
-    $("p#less6").toggle();
-    $("p#more6").toggle();
-  });
-  $("button#button7").click(function(){
-    $("p#less7").toggle();
-    $("p#more7").toggle();
-  });
-  $("button#button8").click(function(){
-    $("p#less8").toggle();
-    $("p.more8").toggle();
-  });
-  $("button#button9").click(function(){
-    $("p#less9").toggle();
-    $("p.more9").toggle();
-  });
+  for(var i=1;i<=9;i++) {
+    $("p.more"+i).hide();
+  }
 });
